3f12ad857ecf5fbfb3fe0492a82d89ff1db6e8a5
waqas-wqs <wa029820@gmail.com>
2025-04-03 15:44:18 +0500

    Who We Are (HTML,CSS,JS)

---
+++ About-Us(Who we are)/who-we-are.js
@@ -0,0 +1,58 @@
+// Define a function to scroll to the top
+function scrollToTop() {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  }
+  
+  // Call the function when the window is loaded
+  window.onload = function () {
+    scrollToTop();
+  };
+  
+  // Call the function when the page is reloaded
+  window.addEventListener('beforeunload', function () {
+    scrollToTop();
+  });
+
+// DONATE BUTTON
+
+window.onscroll = function() {
+    var donateButton = document.getElementById('donateButton');
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        donateButton.classList.add('show');
+    } else {
+        donateButton.classList.remove('show');
+    }
+  };
+
+
+  // CARD FLIP JS
+  document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.service-card');
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            const innerCard = card.querySelector('.service-card-inner');
+            const isFlipped = innerCard.classList.contains('flipped');
+
+            // Remove 'flipped' class from all cards
+            cards.forEach(c => c.querySelector('.service-card-inner').classList.remove('flipped'));
+
+            // Flip the clicked card if it wasn't already flipped
+            if (!isFlipped) {
+                innerCard.classList.add('flipped');
+            }
+        });
+    });
+});
+
+
+// who we are - CARDS SUCCESS STORIES
+
+function toggleStory(cardNumber) {
+  const storyElement = document.getElementById(`story-${cardNumber}`);
+  if (storyElement.style.display === "none" || storyElement.style.display === "") {
+      storyElement.style.display = "block";
+  } else {
+      storyElement.style.display = "none";
+  }
+}
