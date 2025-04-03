b84edd11e6cb88ccc393c4203d5bc1ebbd141296
waqas-wqs <wa029820@gmail.com>
2025-04-03 15:45:56 +0500

    Homepage JS

---
+++ script.js
@@ -0,0 +1,68 @@
+// Define a function to scroll to the top
+function scrollToTop() {
+  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+}
+
+// Call the function when the window is loaded
+window.onload = function () {
+  scrollToTop();
+};
+
+// Call the function when the page is reloaded
+window.addEventListener('beforeunload', function () {
+  scrollToTop();
+});
+
+// COUNTER JS
+
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll('.stat-number');
+  const speed = 200; // Increase this number to make the counter slower
+
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+      const increment = target / speed;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 30);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCount();
+  });
+});
+
+// DONATE BUTTON
+
+window.onscroll = function () {
+  var donateButton = document.getElementById('donateButton');
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    donateButton.classList.add('show');
+  } else {
+    donateButton.classList.remove('show');
+  }
+};
+
+// MOBILE MENU
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('mobile-menu');
+  const navLinks = document.getElementById('nav-links');
+  const dropdowns = document.querySelectorAll('.dropdown');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+
+  dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('click', () => {
+      const dropdownContent = dropdown.querySelector('.dropdown-content');
+      dropdownContent.classList.toggle('active');
+    });
+  });
+});
