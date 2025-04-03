262411d5f9ccb20a450b2f621071ed90ef819f16
waqas-wqs <wa029820@gmail.com>
2025-04-03 15:41:09 +0500

    404 Page (HTML,CSS,JS)

---
+++ 404 Page/404-pages.js
@@ -0,0 +1,55 @@
+// Define a function to scroll to the top
+function scrollToTop() {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  }
+  
+  // Call the function when the window is loaded
+  window.onload = function () {
+    scrollToTop();
+  };
+  
+  // Call the function when the page is reloaded
+  window.addEventListener('beforeunload', function () {
+    scrollToTop();
+  });
+
+// DONATE BUTTON
+
+window.onscroll = function() {
+    var donateButton = document.getElementById('donateButton');
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        donateButton.classList.add('show');
+    } else {
+        donateButton.classList.remove('show');
+    }
+  };
+
+// MOBILE MENU
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('mobile-menu');
+  const navLinks = document.getElementById('nav-links');
+  const dropdowns = document.querySelectorAll('.dropdown');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+
+  dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('click', () => {
+      const dropdownContent = dropdown.querySelector('.dropdown-content');
+      dropdownContent.classList.toggle('active');
+    });
+  });
+});
+
+// 
+
+function toggleContent(element) {
+    const content = element.querySelector('.hidden-content');
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
